c969cf48cd590919f4ec6a35095853d9307e12af
Minwei Xu <faceswilliam@gmail.com>
2017-01-11 01:14:38 +0800

    index.html

--- chatapp/lib/routes/handleRender.js
+++ chatapp/lib/routes/handleRender.js
@@ -6,48 +6,11 @@ const reducers = require('../../src/reducers/index.js').default;
 const ChatApp = require('../../src/components/ChatApp.jsx').default;
 const { chatMiddleware } = require('../../src/chat.js');
 const redis = require('redis');
+const IndexHtml = require('fs').readFileSync('lib/routes/index.html');
+const format = require('es6-template-strings');
 
 function renderFullPage(html, preloadedState) {
-  return `
-    <!doctype html>
-    <html lang="en">
-
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>ReactJS Socket.io Chat Application</title>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-        <link rel="shortcut icon" href="http://chat.mwxu.me/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-
-    <body>
-        <div id="app">${html}</div>
-        <script>
-            (function(i, s, o, g, r, a, m) {
-                i['GoogleAnalyticsObject'] = r;
-                i[r] = i[r] || function() {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date();
-                a = s.createElement(o),
-                    m = s.getElementsByTagName(o)[0];
-                a.async = 1;
-                a.src = g;
-                m.parentNode.insertBefore(a, m)
-            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-            ga('create', 'UA-67391005-2', 'auto');
-            ga('send', 'pageview');
-        </script>
-        <script>
-            window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState)};
-        </script>
-        <script async src="/js/bundle.js"></script>
-    </body>
-
-    </html>`;
+  return format(IndexHtml, { html, preloadedState: JSON.stringify(preloadedState) });
 }
 
 module.exports = function handleRender(req, res) {
