d37e6c1402e8b65409510a668c93e52f6e4cb491
Minwei Xu <faceswilliam@gmail.com>
2017-01-11 12:47:54 +0800

    compression

--- chatapp/lib/index.js
+++ chatapp/lib/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const compression = require('compression');
+
 const socket = require('./routes/socket.js');
 const handleRender = require('./routes/handleRender');
 
@@ -12,6 +14,7 @@ const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
 /* Configuration */
+app.use(compression());
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('port', port);
 app.use(cors());
